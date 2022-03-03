8a9fde505ce7b01f28b66dada387b8e5eba6393b
Lisely Fuenmayor <Lisely.fuenmayor@vasslatam.com>
2022-03-03 18:04:15 -0300

    add build file

---
+++ docs/precache-manifest.37730a4c10e44a6cf3493656ec81d9f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7fd2bce04feebe606086914c50eb82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcdf5ac63af09cf476d9",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcdf5ac63af09cf476d9",
+    "url": "/static/js/main.fd41c26b.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
